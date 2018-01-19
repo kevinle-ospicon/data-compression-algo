fac8ef2814f575207436c617ade4515da0be520b
Kevin Le <kevin.le@ospicon.com>
2018-01-19 15:37:49 +0700

    committing skeleton file for log handler

---
+++ DataCompression/test/test_dev__log_handler.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "dev__log_handler.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_dev__log_handler_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement dev__log_handler");
+}
