848ea46c4be5a9341e55280b5fae9c241fc72aaa
Kevin Le <kevin.le@ospicon.com>
2018-01-24 16:16:21 +0700

    refactor test file.

--- DataCompression/test/test_srv__deserialise.c
+++ DataCompression/test/test_srv__deserialise.c
@@ -26,6 +26,7 @@
 /*----------------------------------------------------------------------------
   type definitions
 ----------------------------------------------------------------------------*/
+typedef void ( * test_payload_cb_t ) ( data__log_packet_t * log_packet );
 
 /*----------------------------------------------------------------------------
   macros
@@ -34,6 +35,9 @@
 /*----------------------------------------------------------------------------
   prototypes
 ----------------------------------------------------------------------------*/
+static void test_raw_adc_payload( data__log_packet_t * log_packet );
+static void test_calibraion_payload( data__log_packet_t * log_packet );
+static void test_temperature_payload( data__log_packet_t * log_packet );
 
 /*----------------------------------------------------------------------------
   global variables
@@ -44,26 +48,56 @@
 ----------------------------------------------------------------------------*/
 static char log_str[ SRV_DESERIALISE_MAX_STRING_LEN ] = "not empty";
 
-// Equivalent Ascii string: "20180119_17:01:01:Temp:22,9"
-static uint8_t temp_packet[] = { 0x0D , 0x0A , 0x3E , 0x3E , 
-                                 0x4D , 0x24 , 0x62 , 0x5A ,
-                                 0x02 , 0x01 , 0x17 };
-static uint8_t correct_begin_marker[] = { 0x0D , 0x0A , 0x3E , 0x3E };
 static uint8_t wrong_begin_marker[] = { 0x0D , 0x0A , 0x3E , 0x00 };
 static uint8_t begin_marker_wrong_LF[] = { 0x0D , 0x00 , 0x0A , 0x3E , 0x3E };
 static uint8_t begin_marker_wrong_begin_1[] = { 0x0D , 0x0A , 0x00 , 0x3E , 0x3E };
 static uint8_t begin_marker_wrong_begin_2[] = { 0x0D , 0x0A , 0x3E , 0x00 , 0x3E };
-static uint8_t correct_header[] = { 0x0D , 0x0A , 0x3E , 0x3E , 
-                                    0x4D , 0x24 , 0x62 , 0x5A , 0x02 , 0x01 };
-static uint8_t cal_packet[] = { 0x0D , 0x0A , 0x3E , 0x3E , 
-                                0x63 , 0x24 , 0x62 , 0x5A , 0x01 , 0x04 , 
-                                0x00 , 0xB0 , 0xC6 , 0x96 };
-static uint8_t raw_adc_packet[] = { 0x0D , 0x0A , 0x3E , 0x3E , 
-                                    0x4F , 0x24 , 0x62 , 0x5A , 0x00 , 0x17 , 
-                                    0x0A , 0xB5 , 0x9F , 0x27 , 0x9E , 0xEF , 
-                                    0x9D , 0x18 , 0x9D , 0x5B , 0x9C , 0xC8 , 
-                                    0x9B , 0x78 , 0x9B , 0x8F , 0x9B , 0xA2 , 
-                                    0x9C , 0x9E , 0x9D , 0x00 , 0x00 };
+
+static uint8_t temp_packet[] = 
+{ 
+    0x0D , 0x0A , 0x3E , 0x3E , 0x4D , 0x24 , 0x62 , 0x5A , 0x02 , 0x01 , 0x17 
+};
+
+static uint8_t correct_header[] = 
+{ 
+    0x0D , 0x0A , 0x3E , 0x3E , 0x4D , 0x24 , 0x62 , 0x5A , 0x02 , 0x01 
+};
+
+static uint8_t cal_packet[] = 
+{ 
+    0x0D , 0x0A , 0x3E , 0x3E , 0x63 , 0x24 , 0x62 , 0x5A , 0x01 , 0x04 , 
+    0x00 , 0xB0 , 0xC6 , 0x96 
+};
+
+static uint8_t raw_adc_packet[] = 
+{ 
+    0x0D , 0x0A , 0x3E , 0x3E , 0x4F , 0x24 , 0x62 , 0x5A , 0x00 , 0x17 , 
+    0x0A , 0xB5 , 0x9F , 0x27 , 0x9E , 0xEF , 0x9D , 0x18 , 0x9D , 0x5B , 
+    0x9C , 0xC8 , 0x9B , 0x78 , 0x9B , 0x8F , 0x9B , 0xA2 , 0x9C , 0x9E , 
+    0x9D , 0x00 , 0x00 
+};
+
+static uint8_t temp_cal_raw_packets[] = 
+{ 
+    // temperature packet
+    0x0D , 0x0A , 0x3E , 0x3E , 0x4D , 0x24 , 0x62 , 0x5A , 0x02 , 0x01 , 
+    0x17 , 
+    // calibration packet
+    0x0D , 0x0A , 0x3E , 0x3E , 0x63 , 0x24 , 0x62 , 0x5A , 0x01 , 0x04 , 
+    0x00 , 0xB0 , 0xC6 , 0x96 ,
+    // raw adc packet 
+    0x0D , 0x0A , 0x3E , 0x3E , 0x4F , 0x24 , 0x62 , 0x5A , 0x00 , 0x17 , 
+    0x0A , 0xB5 , 0x9F , 0x27 , 0x9E , 0xEF , 0x9D , 0x18 , 0x9D , 0x5B , 
+    0x9C , 0xC8 , 0x9B , 0x78 , 0x9B , 0x8F , 0x9B , 0xA2 , 0x9C , 0x9E , 
+    0x9D , 0x00 , 0x00 
+};
+
+static const test_payload_cb_t test_payload_cb [ data__log_type_number_of ] = 
+{
+    test_raw_adc_payload,
+    test_calibraion_payload,
+    test_temperature_payload
+};
 /*----------------------------------------------------------------------------
   public functions
 ----------------------------------------------------------------------------*/
@@ -179,7 +213,7 @@ void test_srv__deserialise_GetTemperaturePacket(void)
 
     data__log_packet_t log_packet = srv__deserialise_get_log_packet();
     
-    TEST_ASSERT_EQUAL_INT8( 23 , log_packet.temperature_payload.value );
+    test_payload_cb[ data__log_type_temperature ]( & log_packet );
 }
 
 /*============================================================================
@@ -197,13 +231,7 @@ void test_srv__deserialise_GetCalibrationPacket(void)
 
     data__log_packet_t log_packet = srv__deserialise_get_log_packet();
 
-    TEST_ASSERT_EQUAL_UINT32( 0x5A622463 , log_packet.header.timestamp );
-    TEST_ASSERT_EQUAL_UINT8( ( uint8_t ) data__log_type_cal , log_packet.header.log_type );
-    TEST_ASSERT_EQUAL_UINT8( sizeof( data__log_cal_payload_t ) , log_packet.header.payload_len );
-
-    TEST_ASSERT_EQUAL_UINT8( data__log_cal_pga_lvl_single_led , log_packet.cal_payload.pga_level );
-    TEST_ASSERT_EQUAL_UINT16( 50864 , log_packet.cal_payload.raw_value );
-    TEST_ASSERT_EQUAL_UINT8( 150 , log_packet.cal_payload.current );
+    test_payload_cb[ data__log_type_cal ]( & log_packet );
 }
 
 /*============================================================================
@@ -221,17 +249,70 @@ void test_srv__deserialise_GetRawAdcPacket(void)
 
     data__log_packet_t log_packet = srv__deserialise_get_log_packet();
 
-    TEST_ASSERT_EQUAL_UINT32( 0x5A62244F , log_packet.header.timestamp );
-    TEST_ASSERT_EQUAL_UINT8( ( uint8_t ) data__log_type_raw_adc , log_packet.header.log_type );
-    TEST_ASSERT_EQUAL_UINT8( sizeof( data__log_raw_adc_payload_t ) , log_packet.header.payload_len );
+    test_payload_cb[ data__log_type_raw_adc ]( & log_packet );
+}
 
-    TEST_ASSERT_EQUAL_UINT8( 0x0A , log_packet.raw_adc_payload.sample_count );
-    TEST_ASSERT_EQUAL_MEMORY( & raw_adc_packet[ 11 ] , log_packet.raw_adc_payload.value , 0x17 );
+/*============================================================================
+@brief
+------------------------------------------------------------------------------
+@note
+============================================================================*/
+void test_srv__deserialise_GetMixedackets(void)
+{
+    for( int idx = 0 ; idx < sizeof( temp_cal_raw_packets ) ; idx ++ )
+    {
+        if( srv__deserialise_parse( temp_cal_raw_packets[ idx ] ) )
+        {
+            data__log_packet_t log_packet = srv__deserialise_get_log_packet();
+
+            test_payload_cb[ log_packet.header.log_type ]( & log_packet );
+        }
+    }
 }
 /*----------------------------------------------------------------------------
   private functions
 ----------------------------------------------------------------------------*/
 
+/*============================================================================
+@brief
+------------------------------------------------------------------------------
+@note
+============================================================================*/
+static void test_raw_adc_payload( data__log_packet_t * log_packet )
+{
+    TEST_ASSERT_EQUAL_UINT32( 0x5A62244F , log_packet->header.timestamp );
+    TEST_ASSERT_EQUAL_UINT8( ( uint8_t ) data__log_type_raw_adc , log_packet->header.log_type );
+    TEST_ASSERT_EQUAL_UINT8( sizeof( data__log_raw_adc_payload_t ) , log_packet->header.payload_len );
+
+    TEST_ASSERT_EQUAL_UINT8( 0x0A , log_packet->raw_adc_payload.sample_count );
+    TEST_ASSERT_EQUAL_MEMORY( & raw_adc_packet[ 11 ] , log_packet->raw_adc_payload.value , 0x17 );
+}
+
+/*============================================================================
+@brief
+------------------------------------------------------------------------------
+@note
+============================================================================*/
+static void test_calibraion_payload( data__log_packet_t * log_packet )
+{
+    TEST_ASSERT_EQUAL_UINT32( 0x5A622463 , log_packet->header.timestamp );
+    TEST_ASSERT_EQUAL_UINT8( ( uint8_t ) data__log_type_cal , log_packet->header.log_type );
+    TEST_ASSERT_EQUAL_UINT8( sizeof( data__log_cal_payload_t ) , log_packet->header.payload_len );
+
+    TEST_ASSERT_EQUAL_UINT8( data__log_cal_pga_lvl_single_led , log_packet->cal_payload.pga_level );
+    TEST_ASSERT_EQUAL_UINT16( 50864 , log_packet->cal_payload.raw_value );
+    TEST_ASSERT_EQUAL_UINT8( 150 , log_packet->cal_payload.current );
+}
+
+/*============================================================================
+@brief
+------------------------------------------------------------------------------
+@note
+============================================================================*/
+static void test_temperature_payload( data__log_packet_t * log_packet )
+{
+    TEST_ASSERT_EQUAL_INT8( 23 , log_packet->temperature_payload.value );
+}
 /*----------------------------------------------------------------------------
   End of file
 ----------------------------------------------------------------------------*/
